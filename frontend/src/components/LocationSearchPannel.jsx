9f2aaeb4b43d2ee36921727022a4bda9c7c5f315
satish chandra <imsachandra@gmail.com>
2024-12-22 02:03:06 +0530

    GSAP

--- frontend/src/components/LocationSearchPannel.jsx
+++ frontend/src/components/LocationSearchPannel.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const LocationSearchPannel = () => {
+const LocationSearchPannel = ({ setVehiclePanel, setPanelOpen }) => {
+  // sample array of locations
+  const locations = [
+    "23, Shanti nagar Bhilai CG",
+    "25, vaishaali nagar Bhilai CG",
+    "1, Gada Chauk Bhilai CG",
+    "2B, kailash nagar Bhilai CG",
+  ];
+
   return (
-    <div>LocationSearchPannel</div>
-  )
-}
+    <div>
+      {/* This is a sample data of location search panel */}
+      {locations.map((elem, idx) => (
+        <div
+          key={idx}
+          onClick={() => {
+            setVehiclePanel(true);
+            setPanelOpen(false);
+          }}
+          className="flex items-center justify-start gap-5 my-3 border-2 p-3 border-gray-50 active:border-black rounded-xl"
+        >
+          <h2 className="bg-[#eeee] h-18  w-12  flex items-center justify-center rounded-full">
+            <i className="ri-map-pin-fill"></i>
+          </h2>
+          <h4 className="text-lg">{elem}</h4>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default LocationSearchPannel
+export default LocationSearchPannel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+// import axios from "axios";
 import "remixicon/fonts/remixicon.css";
-import LocationSearchPannel from "../components/LocationSearchPannel";
+import LocationSearchPanel from "../components/LocationSearchPannel";
+import VehiclePanel from "../components/VehiclePanel";
+import ConfirmedRide from "../components/ConfirmedRide";
 
 const Home = () => {
-  const [pickup, setPickup] = useState("");
   const [destination, setDestination] = useState("");
-  //0- up & 1- bottom
-  const [pannelOpen, setPannelOpen] = useState(false);
-  const PannelRef = useRef(null);
-  const pannelCloseRef = useRef(null);
+  const [panelOpen, setPanelOpen] = useState(false);
+  const vehiclePanelRef = useRef(null);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
+  const ConfirmedRidePanelRef = useRef(null);
+  const [vehiclePanel, setVehiclePanel] = useState(false);
+  const [ConfirmedRidePanel, setConfirmedRidePanel] = useState(false);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -18,82 +23,139 @@ const Home = () => {
 
   useGSAP(
     function () {
-      gsap.to(PannelRef.current, {
-        height: pannelOpen ? "70%" : "0%",
-        duration: 0.5,
-      });
-      gsap.to(pannelCloseRef.current, {
-        rotate: pannelOpen ? 0 : 180,
-        duration: 0.5,
-      });
+      if (panelOpen) {
+        gsap.to(panelRef.current, {
+          height: "70%",
+          padding: 24,
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 1,
+        });
+      } else {
+        gsap.to(panelRef.current, {
+          height: "0%",
+          padding: 0,
+        });
+        gsap.to(panelCloseRef.current, {
+          opacity: 0,
+        });
+      }
     },
-    [pannelOpen]
+    [panelOpen]
   );
-  return (
-    <div className="h-screen relative">
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s"
-        className="w-15 top-5 left-5 absolute"
-        alt="uber_home_logo"
-      />
 
-      <div className="h-screen w-screen ">
-        {/* image for temporaary use  */}
+  useGSAP(
+    function () {
+      if (vehiclePanel) {
+        gsap.to(vehiclePanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(vehiclePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [vehiclePanel]
+  );
+
+  useGSAP(
+    function () {
+      if (ConfirmedRidePanel) {
+        gsap.to(ConfirmedRidePanelRef.current, {
+          transform: "translateY(0)",
+        });
+      } else {
+        gsap.to(ConfirmedRidePanelRef.current, {
+          transform: "translateY(100%)",
+        });
+      }
+    },
+    [ConfirmedRidePanel]
+  );
+
+  return (
+    <div className="h-screen relative overflow-hidden">
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt=""
+      />
+      <div className="h-screen w-screen">
+        {/* image for temporary use  */}
         <img
-          className="h-full w-full object-cover"
-          src="https://images.squarespace-cdn.com/content/v1/54ff63f0e4b0bafce6932642/1613584820445-6MHFT7HI6MHUED46VYU4/Two+Maps+-+Color.png"
+          className="w-full h-full object-cover"
+          src="https://camo.githubusercontent.com/e0debd25d05c84be78d89bf7a2858c65e3cfecd72e95bd22ec50e85fa1f84cfb/68747470733a2f2f322e62702e626c6f6773706f742e636f6d2f2d574f70483738393364526b2f5733527372626f476678492f41414141414141414356552f767a6b39683975526262415777485633366a5455644b4f555552795946322d6167434c63424741732f73313630302f73637265656e73686f74362e706e67"
           alt=""
         />
       </div>
-      <div className=" h-screen absolute top-0  flex flex-col justify-end w-full ">
-        <div className="h-[30%] p-5 bg-white ">
+      <div className=" flex flex-col justify-end h-screen absolute top-0 w-full">
+        <div className="h-[30%] p-6 bg-white relative">
           <h5
-            ref={pannelCloseRef}
-            onClick={(e) => {
-              setPannelOpen(false);
+            ref={panelCloseRef}
+            onClick={() => {
+              setPanelOpen(false);
             }}
-            className="absolute top-6 right-6  text-2xl"
+            className="absolute opacity-0 right-6 top-6 text-2xl"
           >
             <i className="ri-arrow-down-wide-line"></i>
           </h5>
-          <h4 className="text-2xl font-semibold ">Find a Trip</h4>
+          <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form
-            className=""
-            onClick={() => {
-              setPannelOpen(true);
-            }}
+            className="relative py-3"
             onSubmit={(e) => {
               submitHandler(e);
             }}
           >
+            <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full"></div>
             <input
-              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
-              type="text"
               onClick={() => {
-                setPannelOpen(true);
+                setPanelOpen(true);
               }}
-              value={pickup}
-              placeholder="Add a Pickup Location"
-              onChange={(e) => {
-                setPickup(e.target.value);
-              }}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full"
+              type="text"
+              placeholder="Add a pick-up location"
             />
             <input
-              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3"
-              type="text"
-              placeholder="Add a Destination Point"
-              value={destination}
-              onChange={(e) => {
-                setDestination(e.target.value);
+              onClick={() => {
+                setPanelOpen(true);
               }}
+              // value={destination}
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3"
+              type="text"
+              placeholder="Enter your destination"
             />
           </form>
+          <button className="bg-black text-white px-4 py-2 rounded-lg mt-3 w-full">
+            Find Trip
+          </button>
+        </div>
+        <div ref={panelRef} className="bg-white h-0">
+          <LocationSearchPanel
+            setPanelOpen={setPanelOpen}
+            setVehiclePanel={setVehiclePanel}
+            setDestination={setDestination}
+          />
         </div>
       </div>
-      <div ref={PannelRef} className="h-0 bg-red-500">
-        {/* componenets of loaction SearchPanell */}
-        <LocationSearchPannel />
+      <div
+        ref={vehiclePanelRef}
+        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
+      >
+        <VehiclePanel
+          setVehiclePanel={setVehiclePanel}
+          setConfirmedRidePanel={setConfirmedRidePanel}
+        />
       </div>
+      {/* confirm Ride pannel ref */}
+      <div
+        ref={ConfirmedRidePanelRef}
+        className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12"
+      >
+        <ConfirmedRide setConfirmedRidePanel={setConfirmedRidePanel} />
+      </div>
+
+      {/* other ref */}
     </div>
   );
 };
